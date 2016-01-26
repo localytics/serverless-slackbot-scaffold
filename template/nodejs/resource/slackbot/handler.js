33dce257bdbeacbbafefed913504517d2ff39c98
Kevin Deisz <kdeisz@localytics.com>
2016-01-26 13:58:59 -0500

    update to better match slackbot

--- template/nodejs/resource/slackbot/handler.js
+++ template/nodejs/resource/slackbot/handler.js
@@ -11,12 +11,59 @@
 var ServerlessHelpers = require('serverless-helpers-js').loadEnv();
 
 // Require Logic
-var lib = require('../../lib');
 
 // Lambda Handler
 module.exports.handler = function(event, context) {
+  return exports.router(exports.commands())(event, context);
+};
 
-  lib.respond(event, function(error, response) {
-    return context.done(error, response);
-  });
+module.exports.commands = function() {
+  return {
+    ping: function(context, options) {
+      context.succeed({
+        text: 'Hello World',
+        response_type: 'in_channel'
+      });
+    },
+    help: function(context, options) {
+      context.succeed({
+        text: "Here's how to use {{basename}}:",
+        attachments: [
+          {
+            text: 'ping: Ensure this bot is running\n' +
+              'help: Show this help text'
+          }
+        ],
+        response_type: 'ephemeral'
+      });
+    }
+  };
+};
+
+module.exports.router = function (commands) {
+  return function (event, context) {
+    var response;
+    var body = qs.parse(event.body);
+
+    if (!body.token || body.token != process.env.SLACK_VERIFICATION_TOKEN) {
+      return context.fail('Access Denied');
+    }
+
+    if (body.text) {
+      var splitCommand = body.text.split(" ");
+      var command = _.first(splitCommand);
+      var args = _.rest(splitCommand);
+    }
+
+    if (commands[command]) {
+      response = commands[command](context, {
+        args: args,
+        userName: body.user_name
+      });
+    } else {
+      response = context.succeed({ text: 'This is the default response' });
+    }
+
+    return response;
+  };
 };

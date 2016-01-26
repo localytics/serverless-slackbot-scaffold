f99c4786e5fec0b56d598d8243c6371c409dbc0b
Kevin Deisz <kdeisz@localytics.com>
2016-01-26 16:18:18 -0500

    add router tests

--- template/nodejs/test/handler-test.js
+++ template/nodejs/test/handler-test.js
@@ -1,6 +1,6 @@
 var chai = require('chai'),
   expect = chai.expect,
-  handler = require('../resource/slackbot/handler'),
+  slackbot = require('../resource/slackbot/handler'),
   slack = require('localytics-slack'),
   sinon = require('sinon');
 
@@ -8,32 +8,68 @@ chai.use(require('dirty-chai'));
 chai.use(require('sinon-chai'));
 
 describe('handler', function() {
-  var callback, sandbox;
+  describe('subcommands', function() {
+    var callback, sandbox;
 
-  beforeEach(function() {
-    sandbox = sinon.sandbox.create();
-    callback = sandbox.spy();
+    beforeEach(function() {
+      sandbox = sinon.sandbox.create();
+      callback = sandbox.spy();
+    });
+
+    afterEach(function() {
+      sandbox.restore();
+    });
+
+    it('responds to ping', function() {
+      slackbot.ping(null, callback);
+      expect(callback).to.have.been.calledOnce();
+      expect(callback).to.have.been.calledWithExactly(
+        null,
+        slack.inChannelResponse('Hello World')
+      );
+    });
+
+    it('responds to whoami', function() {
+      slackbot.whoami({ userName: 'foobar' }, callback);
+      expect(callback).to.have.been.calledOnce();
+      expect(callback).to.have.been.calledWithExactly(
+        null,
+        slack.ephemeralResponse('foobar')
+      );
+    });
   });
 
-  afterEach(function() {
-    sandbox.restore();
-  });
+  describe('router', function() {
+    var context = {}, sandbox;
 
-  it('responds to ping', function() {
-    handler.ping(null, callback);
-    expect(callback).to.have.been.calledOnce();
-    expect(callback).to.have.been.calledWithExactly(
-      null,
-      slack.inChannelResponse('Hello World')
-    );
-  });
+    beforeEach(function(){
+      sandbox = sinon.sandbox.create();
+      context.done = sandbox.spy();
+    });
 
-  it('responds to whoami', function() {
-    handler.whoami({ userName: 'foobar' }, callback);
-    expect(callback).to.have.been.calledOnce();
-    expect(callback).to.have.been.calledWithExactly(
-      null,
-      slack.ephemeralResponse('foobar')
-    );
+    afterEach(function() {
+      sandbox.restore();
+    });
+
+    it('responds to help', function() {
+      slackbot.handler({
+        'body': {
+          'token': process.env.SLACK_VERIFICATION_TOKEN,
+          'text': 'help'
+        }
+      }, context);
+      expect(context.done).to.have.been.calledOnce();
+      expect(context.done).to.have.been.calledWithExactly({
+        text: "Here's how to use {{basename}}:",
+        attachments: [
+          {
+            text: 'ping: Ping the lambda\n' +
+              'whoami: Figure out who you are\n' +
+              'help: Show this help text'
+          }
+        ],
+        response_type: 'ephemeral'
+      });
+    });
   });
 });

b0dc769dba23efb0e711ce714d69a2c4be1731e4
Kevin Deisz <kdeisz@localytics.com>
2016-01-29 11:08:39 -0500

    more explicit checkout

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
+#!/bin/bash
+
 npm install -g khaos
 khaos install localytics/slackbot-scaffold slackbot
 
 pushd ~/.khaos/slackbot
 if [[ $TRAVIS_PULL_REQUEST == "true" ]]; then
-  git checkout $TRAVIS_COMMIT
+  git fetch origin +refs/pull/$TRAVIS_PULL_REQUEST/merge
+  git checkout -qf FETCH_HEAD
 else
-  git checkout $TRAVIS_BRANCH
+  git checkout $TRAVIS_COMMIT
 fi
 popd
 

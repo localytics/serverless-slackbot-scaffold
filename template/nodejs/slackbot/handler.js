831ff9adb6714e6d14378a338c2945c0dee570d3
Kevin Deisz <kdeisz@localytics.com>
2016-02-18 15:50:50 -0500

    bump localytics slackg

--- template/nodejs/slackbot/handler.js
+++ template/nodejs/slackbot/handler.js
@@ -20,8 +20,8 @@ slackBot.addCommand('ping', 'Ping the lambda', function(options, callback) {
   callback(null, this.inChannelResponse('Hello World'));
 });
 
-slackBot.addCommand('echo', 'Echo the given arguments', function(options, callback) {
-  callback(null, this.ephemeralResponse(options.args.join(' ')));
+slackBot.addCommand('echo words...', 'Echo the given arguments', function(options, callback) {
+  callback(null, this.ephemeralResponse(options.args.words.join(' ')));
 });
 
 // Router configuration

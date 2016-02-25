6203586c19e7598f310f3f0cceaaaeab4bae53a1
Kevin Deisz <kdeisz@localytics.com>
2016-02-25 17:20:29 -0500

    Add eslint

--- template/nodejs/slackbot/handler.js
+++ template/nodejs/slackbot/handler.js
@@ -4,7 +4,7 @@
  * Serverless Module: Lambda Handler
  * - Your lambda functions should be a thin wrapper around your own separate
  * modules, to keep your code testable, reusable and AWS independent
- * - 'serverless-helpers-js' module is required for Serverless ENV var support.  Hopefully, AWS will add ENV support to Lambda soon :)
+ * - 'serverless-helpers-js' module is required for Serverless ENV var support
  */
 
 // Require Serverless ENV vars
@@ -16,11 +16,11 @@ var SlackBot = require('lambda-slack-router');
 // Slack subcommands
 var slackBot = new SlackBot({ token: process.env.SLACK_VERIFICATION_TOKEN });
 
-slackBot.addCommand('ping', 'Ping the lambda', function(options, callback) {
+slackBot.addCommand('ping', 'Ping the lambda', function (options, callback) {
   callback(null, this.inChannelResponse('Hello World'));
 });
 
-slackBot.addCommand('echo words...', 'Echo the given arguments', function(options, callback) {
+slackBot.addCommand('echo words...', 'Echo the given arguments', function (options, callback) {
   callback(null, this.ephemeralResponse(options.args.words.join(' ')));
 });
 

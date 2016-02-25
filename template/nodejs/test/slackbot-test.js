a49114483cc4c33c7ed301e011dcd61c2dfbf103
Kevin Deisz <kdeisz@localytics.com>
2016-02-25 17:20:29 -0500

    add eslint

--- template/nodejs/test/slackbot-test.js
+++ template/nodejs/test/slackbot-test.js
@@ -1,26 +1,28 @@
 var expect = require('chai').expect;
-  slackBot = require('../slackbot/handler').slackBot;
+var slackBot = require('../slackbot/handler').slackBot;
 
-describe('slackbot', function() {
-  var received, receivedArgs, callback = function(error, success) {
+describe('slackbot', function () {
+  var received;
+  var receivedArgs;
+  var callback = function (error, success) {
     received = true;
     receivedArgs = [error, success];
   };
 
-  beforeEach(function() {
+  beforeEach(function () {
     received = false;
     receivedArgs = [];
   });
 
-  it('responds to ping', function() {
+  it('responds to ping', function () {
     slackBot.ping(null, callback);
-    expect(received).to.be.true;
+    expect(received).to.eq(true);
     expect(receivedArgs).to.deep.eq([null, slackBot.inChannelResponse('Hello World')]);
   });
 
-  it('responds to echo', function() {
+  it('responds to echo', function () {
     slackBot.echo({ args: { words: ['one', 'two', 'three'] } }, callback);
-    expect(received).to.be.true;
+    expect(received).to.eq(true);
     expect(receivedArgs).to.deep.eq([null, slackBot.ephemeralResponse('one two three')]);
   });
 });
